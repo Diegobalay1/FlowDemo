46b370bbc56bf79e3b7bc86255a522c1aab6e99d
balay <dlolhd@gmail.com>
2023-02-12 17:44:25 +0100

    Declaring the flow.

--- app/src/main/java/com/example/flowdemo/DemoViewModel.kt
+++ app/src/main/java/com/example/flowdemo/DemoViewModel.kt
@@ -1,7 +1,13 @@
 package com.example.flowdemo
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.*
 
 class DemoViewModel : ViewModel() {
+
+    val myFlow: Flow<Int> = flow {
+        // Producer block
+    }
 
 }
